229d9e4fb21250b63751946304795e55f1ddd5f9
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2024-05-19 19:34:49 +0900

    linked list insert, remove

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # javascript-data-structures-and-algorithms
+
+- Singly Linked List methods:
+    - push
+    - pop
+    - shift
+    - unshift
+    - get
+    - set
+    - insert
+    - remove
+    - reverse
--- singly_linked_list/index.js
+++ singly_linked_list/index.js
@@ -12,7 +12,7 @@ class SinglyLinkedList {
         this.size = 0;
 
         if (Array.isArray(arr) && arr.length > 0) {
-            arr.forEach(element => {
+            arr.forEach((element) => {
                 this.unshift(element);
             });
         }
@@ -32,6 +32,7 @@ class SinglyLinkedList {
         this.size++;
         return this;
     }
+    // remove the node from the end
     pop() {
         if (!this.head) return undefined;
         let current = this.head;
@@ -49,6 +50,7 @@ class SinglyLinkedList {
         }
         return current;
     }
+    // remove the node from the beginning
     shift() {
         if (!this.head) return undefined;
         let secondNode = this.head.next;
@@ -59,6 +61,7 @@ class SinglyLinkedList {
         }
         return secondNode;
     }
+    // add a new node to the beginning
     unshift(val) {
         let newNode = new ListNode(val);
         if (!this.head) {
@@ -74,20 +77,75 @@ class SinglyLinkedList {
     get(index) {
         if (index < 0 || index >= this.size) return null;
         let counter = 0;
-        let current = this.head
-        while(counter !== index){
-            current = current.next
-            counter++
+        let current = this.head;
+        while (counter !== index) {
+            current = current.next;
+            counter++;
         }
-        return current
+        return current;
     }
-    set(index, val){
-        let node = this.get(index)
-        if(node){
-            node.val = val
-            return true
+    set(index, val) {
+        let node = this.get(index);
+        if (node) {
+            node.val = val;
+            return true;
         }
-        return false
+        return false;
+    }
+    // add a node in into certain position
+    insert(index, value) {
+        if (index < 0 || index >= this.size) return false;
+
+        if (index === this.size) {
+            this.push(value);
+            return true;
+        }
+
+        if (index === 0) {
+            this.unshift(value);
+            return true;
+        }
+
+        let prevNode = this.get(index - 1);
+        let newNode = new ListNode(value);
+        let restOfNodes = prevNode.next;
+        prevNode.next = newNode;
+        newNode.next = restOfNodes;
+        this.size++;
+        return true;
+    }
+    // remove a node
+    remove(index) {
+        if (index < 0 || index >= this.size) return undefined;
+        if (index === 0) return this.shift();
+        if (index === this.size - 1) return this.pop();
+        let prev = this.get(index - 1);
+        let curr = prev.next;
+        prev.next = curr.next;
+        this.size--;
+        return curr;
+    }
+    reverse() {
+        let node = this.head;
+        this.head = this.tail;
+        this.tail = node
+        let prev = null;
+        for (let i = 0; i < this.size; i++) {
+            let next = node.next
+            node.next = prev
+            prev = node
+            node = next
+        }
+         return this
+    }
+    print() {
+        let arr = [];
+        let curr = this.head;
+        while (curr) {
+            arr.push(curr.val);
+            curr = curr.next;
+        }
+        console.log(arr);
     }
 }
 const n = new ListNode(5);
@@ -99,6 +157,10 @@ list.push(8);
 list.pop();
 list.shift();
 list.unshift(9);
-list.set(1, 11)
+list.set(1, 11);
+list.insert(12, 3);
+list.remove(1);
+list.reverse();
+list.print()
 // const list2 = new SinglyLinkedList([1,2,3])
 console.log(list);
