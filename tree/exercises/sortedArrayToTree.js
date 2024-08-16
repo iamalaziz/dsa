7de20d451a2331fc12c08c9ab3e908daf8f5d96b
iamalaziz <mashrabovabdulaziz2463@gmail.com>
2024-08-16 21:01:17 +0900

    binary heap: insert()

---
+++ tree/exercises/sortedArrayToTree.js
@@ -0,0 +1,17 @@
+/* https://leetcode.com/problems/convert-sorted-array-to-binary-search-tree/description/ */
+
+
+var sortedArrayToBST = function (nums) {
+    function helper(start, end) {
+        if (start > end) {
+            return null;
+        }
+        const mid = Math.floor((start + end) / 2);
+        const root = new TreeNode(nums[mid]);
+        root.left = helper(start, mid - 1);
+        root.right = helper(mid + 1, end);
+
+        return root;
+    }
+    return helper(0, nums.length - 1);
+};
