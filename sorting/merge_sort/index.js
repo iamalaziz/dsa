2cd11bb9d63c63c245ed6f32a8ac0c8a21953b83
abdulaziz <codexdev3@gmail.com>
2025-03-16 13:03:00 +0900

    sorting: merge sort

--- sorting/merge_sort/index.js
+++ sorting/merge_sort/index.js
@@ -1,39 +1,65 @@
-const merge = (arr1, arr2) => {
-    let i = 0,
-        j = 0,
-        len1 = arr1.length,
-        len2 = arr2.length;
-    let res = [];
+/**
+ * Merges two arrays.
+ * @param {Array} arr1 an array of numbers to merge.
+ * @param {Array} arr2 an array of numbers to merge.
+ * @param {Function} comparator a function to compare two elements.
+ * @returns Merged array.
+ */
+const merge = (arr1, arr2, comparator) => {
+    if (typeof comparator !== 'function') {
+        comparator = (a, b) => a - b;
+    }
 
-    while (i < len1 && j < len2) {
-        if (arr1[i] > arr2[j]) {
-            res.push(arr2[j]);
+    let i = 0,
+        j = 0;
+    let result = [];
+
+    while (i < arr1.length && j < arr2.length) {
+        if (comparator(arr1[i], arr2[j]) > 0) {
+            result.push(arr2[j]);
             j++;
         } else {
-            res.push(arr1[i]);
+            result.push(arr1[i]);
             i++;
         }
     }
 
-    while (i < len1) {
-        res.push(arr1[i]);
+    while (i < arr1.length) {
+        result.push(arr1[i]);
         i++;
     }
 
-    while (j < len2) {
-        res.push(arr2[j]);
+    while (j < arr2.length) {
+        result.push(arr2[j]);
         j++;
     }
 
-    return res;
+    return result;
 };
 
-const mergeSort = (arr) => {
+// console.log(merge([1, 10, 50], [1, 2, 14, 99, 100]));
+
+/**
+ * Sorts an array with `Merge sorting` algorithm.
+ *
+ * Time complexity: O(n log n)
+ *
+ * Space complexity: O(n)
+ *
+ * @param {Array} arr an array of numbers to sort.
+ * @param {Function} comparator a function to compare two elements.
+ * @returns Sorted array.
+ */
+const mergeSort = (arr, comparator) => {
+    if (typeof comparator !== 'function') {
+        comparator = (a, b) => a - b;
+    }
+
     if (arr.length <= 1) return arr;
     let mid = Math.floor(arr.length / 2);
-    let left = mergeSort(arr.slice(0, mid));
-    let right = mergeSort(arr.slice(mid));
-    return merge(left, right);
+    let left = mergeSort(arr.slice(0, mid), comparator);
+    let right = mergeSort(arr.slice(mid), comparator);
+    return merge(left, right, comparator);
 };
-// console.log(merge([1, 10, 50], [1, 2, 14, 99, 100]));
+
 console.log(mergeSort([1, 10, 50, 1, 2, 14, 99, 100]));
